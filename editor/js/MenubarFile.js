2056bcba23fb053f8714a4b48e2113cbb5cdd470
kenji <k.saito.1985@gmail.com>
2020-07-02 15:16:41 +0900

    add PanelDownload

--- editor/js/MenubarFile.js
+++ editor/js/MenubarFile.js
@@ -2,93 +2,80 @@
  * @author mrdoob / http://mrdoob.com/
  */
 
-function MenubarFile( editor ) {
-
+function MenubarFile(editor) {
 	var signals = editor.signals;
 
 	var container = new UI.Panel();
-	container.setClass( 'menu' );
+	container.setClass('menu');
 
 	var title = new UI.Panel();
-	title.setClass( 'title' );
-	title.setTextContent( 'File' );
-	container.add( title );
+	title.setClass('title');
+	title.setTextContent('File');
+	container.add(title);
 
 	var options = new UI.Panel();
-	options.setClass( 'options' );
-	container.add( options );
+	options.setClass('options');
+	container.add(options);
 
 	// New
 
 	var option = new UI.Row();
-	option.setClass( 'option' );
-	option.setTextContent( 'New' );
-	option.onClick( function () {
-
-		if ( confirm( 'Any unsaved data will be lost. Are you sure?' ) ) {
-
+	option.setClass('option');
+	option.setTextContent('New');
+	option.onClick(function () {
+		if (confirm('Any unsaved data will be lost. Are you sure?')) {
 			editor.clear();
-
 		}
-
-	} );
-	options.add( option );
+	});
+	options.add(option);
 
 	// import
 
 	var option = new UI.Panel();
-	option.setClass( 'option' );
-	option.setTextContent( 'Import' );
-	option.onClick( Import );
-	options.add( option );
+	option.setClass('option');
+	option.setTextContent('Import');
+	option.onClick(Import);
+	options.add(option);
 
-	var fileInput = document.createElement( 'input' );
+	var fileInput = document.createElement('input');
 	fileInput.type = 'file';
-	fileInput.addEventListener( 'change', function ( event ) {
-
+	fileInput.addEventListener('change', function (event) {
 		var reader = new FileReader();
-		reader.addEventListener( 'load', function ( event ) {
+		reader.addEventListener(
+			'load',
+			function (event) {
+				editor.clear();
+				editor.fromJSON(JSON.parse(event.target.result));
+			},
+			false
+		);
 
-			editor.clear();
-			editor.fromJSON( JSON.parse( event.target.result ) );
+		reader.readAsText(fileInput.files[0]);
+	});
 
-		}, false );
-
-		reader.readAsText( fileInput.files[ 0 ] );
-
-	} );
-
-	function Import () {
-
-		if ( confirm( 'Any unsaved data will be lost. Are you sure?' ) )
-			fileInput.click();
-
+	function Import() {
+		if (confirm('Any unsaved data will be lost. Are you sure?')) fileInput.click();
 	}
 
 	// export
 
-	var option = new UI.Panel();
-	option.setClass( 'option' );
-	option.setTextContent( 'Export' );
-	option.onClick( Export );
-	options.add( option );
+	var option = new UI.DownloadPanel();
+	option.setClass('option');
+	option.setTextContent('Export');
+	option.onClick(Export);
+	options.add(option);
 
-	signals.exportState.add( Export );
+	signals.exportState.add(Export);
 
-	function Export () {
-
-		var output = JSON.stringify( editor.toJSON(), null, '\t' );
-
-		var blob = new Blob( [ output ], { type: 'text/plain' } );
-		var objectURL = URL.createObjectURL( blob );
-
-		window.open( objectURL, '_blank' );
-		window.focus();
-
+	function Export() {
+		const filename = 'project.json';
+		const link = option.dom;
+		const output = JSON.stringify(editor.toJSON(), null, '\t');
+		link.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(output));
+		link.setAttribute('download', filename);
 	}
 
 	return container;
-
 }
 
 export { MenubarFile };
